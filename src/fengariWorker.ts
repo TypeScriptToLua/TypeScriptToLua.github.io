99023da164b44d2aed206d936be0da095ec69cea
Lolleko <larry.junglas@hotmail.de>
2018-10-13 23:28:54 +0200

    Updates

--- src/fengariWorker.ts
+++
@@ -1,5 +0,0 @@
-//import {lauxlib, lua, lualib, to_jsstring, to_luastring } from "fengari";
-
-onmessage = (event: MessageEvent) => {
-    //postMessage({luaStr: transpileString(event.data.tsStr)});
-};
