63c66e2f93eafd3ed1f0d496a255790fc9ea5ec0
Perryvw <perry.wesel@gmail.com>
2019-04-30 21:04:25 +0200

    Fixed playground example indenting

--- src/playground/index.ts
+++ src/playground/index.ts
@@ -37,33 +37,33 @@ document.addEventListener('DOMContentLoaded', () => {
   // Actual editor and transpilation
   let example = `/** @noSelfInFile */
 
-  // Declare exposed API
-  type Vector = [number, number, number];
-  
-  declare interface OnSpellStartEvent {
-      caster: Unit;
-      targetLocation: Vector;
-  }
-  
-  declare class Unit {
-      getLevel(): number;
-      isEnemy(other: Unit): boolean;
-      kill(): void;
-  }
-  
-  declare function print(...messages: any[]): void;
-  declare function FindUnitsInRadius(location: Vector, radius: number): Unit[];
-  
-  // Use declared API in code
-  function onSpellStart(event: OnSpellStartEvent): void {
-      const units = FindUnitsInRadius(event.targetLocation, 500);
-      const enemies = units.filter(unit => event.caster.isEnemy(unit));
-  
-      for (const unit of enemies) {
-          print(unit, unit.getLevel());
-          unit.kill();
-      }
-  }`;
+// Declare exposed API
+type Vector = [number, number, number];
+
+declare interface OnSpellStartEvent {
+    caster: Unit;
+    targetLocation: Vector;
+}
+
+declare class Unit {
+    getLevel(): number;
+    isEnemy(other: Unit): boolean;
+    kill(): void;
+}
+
+declare function print(...messages: any[]): void;
+declare function FindUnitsInRadius(location: Vector, radius: number): Unit[];
+
+// Use declared API in code
+function onSpellStart(event: OnSpellStartEvent): void {
+    const units = FindUnitsInRadius(event.targetLocation, 500);
+    const enemies = units.filter(unit => event.caster.isEnemy(unit));
+
+    for (const unit of enemies) {
+        print(unit, unit.getLevel());
+        unit.kill();
+    }
+}`;
   
   var queryStringSrcStart = window.location.hash.indexOf("#src=");
   if (queryStringSrcStart == 0) {
