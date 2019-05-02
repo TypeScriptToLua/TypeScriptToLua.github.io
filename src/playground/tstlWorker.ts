c9e84b04b9ac455b005a4a053ffb129f2b0da88c
Lolleko <larry.junglas@hotmail.de>
2019-05-02 16:25:01 +0200

    Added build time compression, removed debug print

--- src/playground/tstlWorker.ts
+++ src/playground/tstlWorker.ts
@@ -3,57 +3,13 @@ import {LuaTranspiler, TranspileResult} from "typescript-to-lua/dist/LuaTranspil
 
 import * as ts from "typescript";
 
-// TODO this needs to be improved it's a mess;
-const luaLib: { [key: string]: string } = {
-    ArrayConcat: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ArrayConcat.lua"),
-    ArrayEvery: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ArrayEvery.lua"),
-    ArrayFilter: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ArrayFilter.lua"),
-    ArrayForEach: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ArrayForEach.lua"),
-    ArrayIndexOf: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ArrayIndexOf.lua"),
-    ArrayMap: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ArrayMap.lua"),
-    ArrayPush: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ArrayPush.lua"),
-    ArrayReverse: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ArrayReverse.lua"),
-    ArrayShift: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ArrayShift.lua"),
-    ArrayUnshift: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ArrayUnshift.lua"),
-    ArraySort: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ArraySort.lua"),
-    ArraySlice: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ArraySlice.lua"),
-    ArraySome: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ArraySome.lua"),
-    ArraySplice: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ArraySplice.lua"),
-    ArrayFlat: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ArrayFlat.lua"),
-    ArrayFlatMap: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ArrayFlatMap.lua"),
-    ArraySetLength: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ArraySetLength.lua"),
-    ClassIndex: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ClassIndex.lua"),
-    ClassNewIndex: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ClassNewIndex.lua"),
-    FunctionApply: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/FunctionApply.lua"),
-    FunctionBind: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/FunctionBind.lua"),
-    FunctionCall: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/FunctionCall.lua"),
-    Index: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/Index.lua"),
-    InstanceOf: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/InstanceOf.lua"),
-    Iterator: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/Iterator.lua"),
-    Map: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/Map.lua"),
-    NewIndex: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/NewIndex.lua"),
-    ObjectAssign: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ObjectAssign.lua"),
-    ObjectEntries: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ObjectEntries.lua"),
-    ObjectFromEntries: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ObjectFromEntries.lua"),
-    ObjectKeys: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ObjectKeys.lua"),
-    ObjectValues: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/ObjectValues.lua"),
-    Set: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/Set.lua"),
-    WeakMap: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/WeakMap.lua"),
-    WeakSet: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/WeakSet.lua"),
-    SourceMapTraceBack: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/SourceMapTraceBack.lua"),
-    StringReplace: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/StringReplace.lua"),
-    StringSplit: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/StringSplit.lua"),
-    StringConcat: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/StringConcat.lua"),
-    Symbol: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/Symbol.lua"),
-    SymbolRegistry: require("raw-loader!../../node_modules/typescript-to-lua/dist/lualib/SymbolRegistry.lua"),
-}
-
 declare var self: any;
 
+/** Dummy fs for lualib compatibility */
 self.fs = {
     readFileSync: (fileName: string) => {
         let featureName = fileName.replace("/dist/lualib/", "").replace(".lua", "");
-        return new Buffer(luaLib[featureName]);
+        return new Buffer(require(`raw-loader!../../node_modules/typescript-to-lua/dist/lualib/${featureName}.lua`));
     }
 }
 
@@ -76,7 +32,6 @@ function transpileString(str: string, options: CompilerOptions = {
     getNewLine: () => '\n',
 
     getSourceFile: (filename: string, languageVersion: any) => {
-      console.log(filename);
       if (filename === 'file.ts') {
         return ts.createSourceFile(
             filename, str, ts.ScriptTarget.Latest, false);
