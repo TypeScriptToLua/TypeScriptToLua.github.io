0c248365b9b79cb98cda5fd4ade7afa84b94739c
ark120202 <ark120202@gmail.com>
2020-01-24 22:57:19 +0000

    Upgrade dependencies

--- src/webpack.common.js
+++ src/webpack.common.js
@@ -8,13 +8,11 @@ module.exports = {
       {test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/},
       {test: /\.css$/, use: ['style-loader', 'css-loader']}, {
         test: /\.(png|svg|jpg|gif|ico)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {name: '[path][name].[ext]?hash=[hash:20]', limit: 8192}
-        }]
+        loader: 'url-loader',
+        options: {esModule: false, name: '[path][name].[ext]?hash=[hash:20]', limit: 8192},
       },
       {test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']},
-      {test: /\.webmanifest?$/, use: 'file-loader'},
+      {test: /\.webmanifest?$/, loader: 'file-loader', options: {esModule: false}},
     ]
   },
   plugins: [],
