cc4202bf5dfbe979051cb0da484aef482856fb29
ark120202 <ark120202@gmail.com>
2020-01-26 03:46:29 +0000

    Don't include unused highlight.js grammars

--- src/landing/index.ts
+++ src/landing/index.ts
@@ -1,8 +1,10 @@
-import "../../assets/styles/landing.scss";
+import * as hljs from "highlight.js/lib/highlight";
+import * as lua from "highlight.js/lib/languages/lua";
+import * as typescript from "highlight.js/lib/languages/typescript";
 import "highlight.js/styles/vs2015.css";
+import "../../assets/styles/landing.scss";
 
-import { configure, highlightBlock } from "highlight.js";
+hljs.registerLanguage("lua", lua);
+hljs.registerLanguage("typescript", typescript);
 
-configure({ languages: ["typescript", "lua"] });
-
-document.querySelectorAll(".example-item").forEach(highlightBlock);
+document.querySelectorAll(".example-item").forEach(hljs.highlightBlock);
--- src/types.d.ts
+++ src/types.d.ts
@@ -7,3 +7,4 @@ declare module "worker-loader!*" {
 }
 
 declare module "fengari-web";
+declare module "highlight.js/lib/*";
