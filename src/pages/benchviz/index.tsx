99e724f6cad77ed8f2d441045837f6304ffbfd23
Perryvw <perry.wesel@gmail.com>
2020-06-10 11:05:34 +0200

    removed zlib and nested layout

--- src/pages/benchviz/index.tsx
+++ src/pages/benchviz/index.tsx
@@ -1,16 +1,13 @@
-import Layout from "@theme/Layout";
 import React from "react";
 import Benchmark from "./Benchmark";
 
 export default function CreateBenchmark() {
     const isSSR = typeof window === "undefined";
     return (
-        <Layout title="Benchmark">
-            {!isSSR && (
-                <React.Suspense fallback={<div />}>
-                    <Benchmark />
-                </React.Suspense>
-            )}
-        </Layout>
+        !isSSR && (
+            <React.Suspense fallback={<div />}>
+                <Benchmark />
+            </React.Suspense>
+        )
     );
 }
