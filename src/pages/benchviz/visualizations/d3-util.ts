dbbc7d062d84a49ba6097d607721943bca66f06b
Perryvw <perry.wesel@gmail.com>
2020-06-10 10:56:16 +0200

    Small PR feedback things

--- src/pages/benchviz/visualizations/d3-util.ts
+++ src/pages/benchviz/visualizations/d3-util.ts
@@ -6,22 +6,22 @@ export function addLegend(
 ) {
     const legend = selection.append("g");
 
-    items.forEach(([name, color], i) => {
+    for (const [index, [name, color]] of items.entries()) {
         legend
             .append("rect")
             .attr("width", 15)
             .attr("height", 15)
-            .attr("x", 100 * i)
+            .attr("x", 100 * index)
             .style("fill", color)
             .style("stroke", "currentColor");
 
         legend
             .append("text")
-            .attr("x", 100 * i + 20)
+            .attr("x", 100 * index + 20)
             .attr("y", 13)
             .text(name)
             .attr("fill", "currentColor");
-    });
+    }
 
     return legend;
 }
